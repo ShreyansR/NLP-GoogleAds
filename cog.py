b449614a184aa2999274c263b55b00e2cbf9a85f
ShreyansR <shreyans.rishi@uoit.net>
2019-07-03 11:19:35 -0400

    Output Clean Text

--- cog.py
+++ cog.py
@@ -30,7 +30,7 @@ def com() -> str:
     chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
     text = '\n'.join(chunk for chunk in chunks if chunk)
 
-    return text
+    return text[273:]
 
 
 if __name__ == "__main__":
