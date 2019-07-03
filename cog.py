8a8e6fa2c58eb152608826a1c105b1e64f6676ca
shreyans.rishi <shreyans.rishi@loblaw.ca>
2019-07-03 10:41:05 -0400

    Split Lines

--- cog.py
+++ cog.py
@@ -16,7 +16,9 @@ def com() -> None:
         script.decompose()
 
     text = soup.get_text("|", strip=True)
-    print(text)
 
-    # analysis = response.json()
-    # print(json.dumps(analysis))
+    lines = (line.strip() for line in text.splitlines())
+
+    chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
+    text = '\n'.join(chunk for chunk in chunks if chunk)
+    print(text)
